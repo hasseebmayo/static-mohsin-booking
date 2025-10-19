37016c3e13484f01d76e1f3a98e51e96b25dc159
Haseeb Ahmad <hasseebmayo@gmail.com>
2025-10-20 03:27:51 +0500

    logo issye is resloved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
           {/* Logo Section */}
           <div className="flex items-center gap-3 flex-shrink-0">
             <img
-              src="/src/assets/logo.png"
+              src="/logo.png"
               alt="Octans Care Logo"
               className="h-10 w-auto"
             />
